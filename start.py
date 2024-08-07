ef4dd52436c8404b0bf22f0d60d37b865bdec244
Boitumelo <boityy.js@gmail.com>
2024-08-07 13:22:46 +0200

    corrected time error

--- start.py
+++ start.py
@@ -15,24 +15,25 @@ def generate_problem():
     answer = eval(expr)
     return expr, answer
 
-
 wrong = 0
 input("Press enter to start!")
 print("----------------------")
 
 start_time = time.time()
 
-
 for i in range(TOTAL_PROBLEMS):
     expr, answer = generate_problem()
     while True:
-     guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
-     if guess == str(answer):
-        break
-     wrong += 1
+        guess = input(f"Problem #{i + 1}: {expr} = ")
+        if guess == str(answer):
+            break
+        else:
+            print("Incorrect! Try again.")
+            wrong += 1
 
-     end_time = time.time()
+end_time = time.time()
 total_time = round(end_time - start_time, 2)
 
 print("----------------------")
 print("Great job! You finished in", total_time, "seconds!")
+print("Total incorrect answers:", wrong)
